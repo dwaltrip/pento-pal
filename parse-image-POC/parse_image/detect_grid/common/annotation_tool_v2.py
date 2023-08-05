0e2db767a1683eda72c75eb8f94b0b4009c8caa2
Daniel Waltrip <dwaltrip77@gmail.com>
2023-08-05 12:18:37 -0700

    WIP -- add more progress info to annotation tool

--- parse-image-POC/parse_image/detect_grid/common/annotation_tool_v2.py
+++ parse-image-POC/parse_image/detect_grid/common/annotation_tool_v2.py
@@ -19,21 +19,21 @@ from parse_image.utils.misc import (
 LABEL_VIZ = SimpleNamespace(
     rows=GRID.height,
     cols=GRID.width,
-    square_size=60, # pixels
-    padding=5, # pixels
-    padding_color='#808080',
-    scale_factor=0.9, # account for the bg of the training images
+    cell_size=60,
+    cell_padding=5,
+    cell_padding_color='#808080',
+    # scale_factor=0.9, # account for the bg of the training images
 )
-def get_img_size(rows, cols, square_size, padding):
+def get_img_size(rows, cols, cell_size, padding):
     return SimpleNamespace(
-        height=(square_size+padding)*rows + padding,
-        width=(square_size+padding)*cols + padding,
+        height=(cell_size + padding) * rows + padding,
+        width=(cell_size + padding) * cols + padding,
     )
 VIZ_IMG_SIZE = get_img_size(
     rows=LABEL_VIZ.rows,
     cols=LABEL_VIZ.cols,
-    square_size=LABEL_VIZ.square_size,
-    padding=LABEL_VIZ.padding,
+    cell_size=LABEL_VIZ.cell_size,
+    padding=LABEL_VIZ.cell_padding,
 )
 
 
@@ -60,6 +60,9 @@ def pad_img(img, target_size, fill=(0,0,0)):
     pad_fn = torchvision.transforms.Pad(padding, fill=fill)
     return np.array(pad_fn(Image.fromarray(img)))
 
+
+# NOTE: Not using this currently...
+# May want something like this if we attempt the hard method again.
 def combine_images(img1, img2, padding=20, bg_color='#000'):
     height1, height2 = img1.shape[0], img2.shape[0]
 
@@ -83,16 +86,27 @@ def combine_images(img1, img2, padding=20, bg_color='#000'):
     return np.hstack((img1, horiz_padding_img, img2))
 
 
-def validate_labels(label_path_or_content):
+def load_label_or_noop(label_path_or_content):
     if os.path.exists(label_path_or_content):
         with open(label_path_or_content, 'r') as f:
-            lines = f.read().splitlines()
-    else:
-        lines = label_path_or_content.splitlines()
+            return f.read()
+    return label_path_or_content
 
-    labels = [line for line in lines if line and line[0] != '#']
-    flat_labels = ''.join(labels)
-    return all(flat_labels.count(c) == 5 for c in CLASS_NAMES)
+
+def get_class_counts(labels):
+    content = load_label_or_noop(labels)
+    lines = content.splitlines()
+    flat_labels = ''.join([
+        line.lower() for line in lines if (line and line[0] != '#')
+    ])
+    return { c: flat_labels.count(c) for c in CLASS_NAMES }
+
+def validate_labels(labels):
+    content = load_label_or_noop(labels)
+    if len(content.strip()) == 0:
+        return False
+    counts = get_class_counts(content)
+    return all(count == 5 for name, count in counts.items())
 
 
 def make_textured_color_block(size, base_color): 
@@ -115,18 +129,18 @@ def visualize_labels(label_path):
     img_size = (VIZ_IMG_SIZE.height, VIZ_IMG_SIZE.width, 3)
     viz_img = np.full(
         img_size,
-        hex_to_cv2_color(LABEL_VIZ.padding_color),
+        hex_to_cv2_color(LABEL_VIZ.cell_padding_color),
         dtype=np.uint8,
     )
 
-    size = LABEL_VIZ.square_size
-    padding = LABEL_VIZ.padding
+    size = LABEL_VIZ.cell_size
+    cell_padding = LABEL_VIZ.cell_padding 
 
     for i in range(LABEL_VIZ.rows):
         for j in range(LABEL_VIZ.cols):
             top_left = dict(
-                y = i * (size + padding) + padding,
-                x = j * (size + padding) + padding,
+                y = i * (size + cell_padding) + cell_padding,
+                x = j * (size + cell_padding) + cell_padding,
             )
             y = (top_left['y'], top_left['y'] + size)
             x = (top_left['x'], top_left['x'] + size)
@@ -138,9 +152,13 @@ def visualize_labels(label_path):
             )
             viz_img[y[0]:y[1], x[0]:x[1]] = color_block
 
-    s = LABEL_VIZ.scale_factor
-    scaled_size = (int(VIZ_IMG_SIZE.width * s), int(VIZ_IMG_SIZE.height * s))
-    return cv2.resize(viz_img, scaled_size)
+    return viz_img
+    # # -----------------------------------------------------------
+    # NOTE: For the "easy method", we don't need this.
+    #   If we attempt the hard method again, we may want it back.
+    # s = LABEL_VIZ.scale_factor
+    # scaled_size = (int(VIZ_IMG_SIZE.width * s), int(VIZ_IMG_SIZE.height * s))
+    # return cv2.resize(viz_img, scaled_size)
 
 
 def normalize_label_file_content(content):
@@ -171,14 +189,44 @@ def normalize_label_file(label_path):
         file.write(normalize_label_file_content(content))
 
 
-def label_images(image_dir, label_dir):
-    Path(label_dir).mkdir(parents=True, exist_ok=True)
+def get_label_filename(image_filename):
+    return os.path.splitext(image_filename)[0] + '.txt'
 
-    for i, filename in enumerate(os.listdir(image_dir)):
-        if not is_image(filename):
-            continue
+def images_in_dir(dirpath):
+    return [f for f in os.listdir(dirpath) if is_image(f)]
 
-        label_filename = os.path.splitext(filename)[0] + '.txt'
+def is_label_valid(label_path):
+    normalize_label_file(label_path)
+    return validate_labels(label_path)
+
+
+def label_images(data_dir):
+    image_dir = os.path.join(data_dir, 'images')
+    label_dir = os.path.join(data_dir, 'labels')
+    assert os.path.exists(image_dir), f'Image dir does not exist: {image_dir}'
+    assert os.path.exists(label_dir), f'Label dir does not exist: {label_dir}'
+
+    image_files = images_in_dir(image_dir)
+
+    total_images_count = len(image_files)
+    labeled_images_count = len([
+        True for f in image_files if validate_labels(
+            os.path.join(label_dir, get_label_filename(f))
+        )
+    ])
+
+    labeled_in_this_session = 0
+    labels_to_go = total_images_count - labeled_images_count
+
+    print('## Annotation Tool ##')
+    print('---------------------')
+    print('Total images:', total_images_count)
+    print('Labeled:', labeled_images_count)
+    print('Unlabled:', labels_to_go)
+    print('---------------------')
+
+    for i, image_filename in enumerate(image_files):
+        label_filename = get_label_filename (image_filename)
         label_path = os.path.join(label_dir, label_filename)
 
         if os.path.exists(label_path):
@@ -187,14 +235,15 @@ def label_images(image_dir, label_dir):
                 print('Skipping... Valid existing labels found:', label_filename)
                 continue
 
-        img_path = os.path.join(image_dir, filename)
+        img_path = os.path.join(image_dir, image_filename)
 
         # Load and resize the image, then display it
         img = cv2.imread(img_path)
         img = cv2.resize(img, (VIZ_IMG_SIZE.width, VIZ_IMG_SIZE.height))
 
-        cv2.namedWindow('training-img', cv2.WINDOW_NORMAL)
-        cv2.imshow('training-img', img)
+        window_name = f'Image to Label (completed so far: {labeled_in_this_session}/{labels_to_go})'
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        cv2.imshow(window_name, img)
         cv2.waitKey(1)
 
         while True:
@@ -205,9 +254,12 @@ def label_images(image_dir, label_dir):
                 with open(label_path, 'r+') as f:
                     content = f.read()
                     f.seek(0, 0)
+                    counts = get_class_counts(content)
+                    invalid_classes = [name for name, count in counts.items() if count != 5]
                     error_msg = '\n'.join([
                         '# ERROR: Invalid label file!',
                         '# It should have exactly 5 occurrences of each class.',
+                        f'# {", ".join(invalid_classes)} need to be fixed.',
                         '# Please correct the labels.',
                         '# --------------------------',
                     ])
@@ -216,32 +268,34 @@ def label_images(image_dir, label_dir):
 
             label_viz_img = visualize_labels(label_path)
             cv2.destroyAllWindows()
-            combined_img = combine_images(img, label_viz_img, bg_color='#ba8')
+            combined_img = np.hstack((img, label_viz_img))
 
-            cv2.namedWindow('original-plus-label-viz', cv2.WINDOW_NORMAL)
-            cv2.imshow('original-plus-label-viz', combined_img)
+            num_left = labels_to_go - (labeled_in_this_session+1)
+            window_name = f'Verification ({num_left} to go!)'
+            cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+            cv2.imshow(window_name, combined_img)
             cv2.waitKey(1)
 
             user_input = input(' '.join([
                 'If visual verification is correct, press ENTER.',
                 'If not, press any other key to edit labels: ',
             ]))
+            # This means the label was valid
             if user_input == '':
+                labeled_in_this_session += 1
                 break
 
         cv2.destroyAllWindows()
-        print('Saved labels for ' + filename)
+        print('Saved labels for ' + image_filename)
 
     print('Finished labeling all images')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python annotation_tool_v2.py <image_dir> <label_dir>')
+    if len(sys.argv) != 2:
+        print('Usage: python annotation_tool_v2.py <data_dir>')
         sys.exit(1)
 
-    image_dir = sys.argv[1]
-    label_dir = sys.argv[2]
-    print('image_dir:', image_dir)
-    print('label_dir:', label_dir)
-    label_images(image_dir, label_dir)
+    data_dir = sys.argv[1]
+    print('data_dir:', data_dir)
+    label_images(data_dir)
