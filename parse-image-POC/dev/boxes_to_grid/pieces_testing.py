b3e96a38c0dc7a8bb533474cb4b66d80503a9221
Daniel Waltrip <dwaltrip77@gmail.com>
2023-08-09 06:10:53 -0700

    Parse Piece obj from string. Generate all orientations.

---
+++ parse-image-POC/dev/boxes_to_grid/pieces_testing.py
@@ -0,0 +1,11 @@
+from parse_image.boxes_to_grid.pieces import *
+
+
+if __name__ == '__main__':
+    for piece in PIECES:
+        print('----------------------')
+        print('name:', piece.name)
+        
+        for i, variant in enumerate(piece.variants):
+            print('\tvariant', i+1)
+            Piece._print_grid(variant, prefix='\t')
